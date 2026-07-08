cb209e9f39a20bd890dbc4f77648689a3eabbc3b
Gregory Haas <gregoryjedha@gmail.com>
2026-07-08 16:30:23 +0200

    Modify ci

--- tests/test_root.py
+++ tests/test_root.py
@@ -2,7 +2,7 @@ import pytest
 import httpx
 
 def test_root():
-    response = httpx.get("http://localhost:8090/")
+    response = httpx.get("http://localhost:8090")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
 
